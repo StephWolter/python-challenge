e657da65cfc41ee31bd6a82eed74a1902d253364
stephanie.wolter@gmail.com <stephanie.wolter@gmail.com>
2023-03-13 09:51:43 -0400

    almost done pypoll

--- PyPoll/main_poll.py
+++ PyPoll/main_poll.py
@@ -3,6 +3,9 @@ import csv
 
 election_csv = os.path.join("Resources", "election_data.csv")
 election_data = []
+candidate_list = {}
+vote_candidates = ()
+
 # Open and read csv
 with open(election_csv) as csvfile:
     election_data = csv.reader(csvfile, delimiter=",")
@@ -20,44 +23,44 @@ with open(election_csv) as csvfile:
     for row_data in election_data:
         ballot_id, county, candidate_voted = row_data[0], row_data[1], row_data[2]
         #spot check those lists worked
-        #print(candidate_voted)
 
         #The total number of votes cast
         total_votes += 1
 
-        # A complete list of candidates who received votes
+        # A complete list of candidates who received votes and the running total
         if candidate_voted not in candidate_list:
             candidate_list.append(candidate_voted)
             votes[candidate_voted] = 0
         votes[candidate_voted] += 1
-
+        #print(candidate_list)
+    
     
     #The total number of votes each candidate won
-    CCS_votes = float(votes["Charles Casper Stockham"])
-    DD_votes = float(votes["Diana DeGette"])
-    RAD_votes = float(votes["Raymon Anthony Doane"])
+    CCS_votes = int(votes[candidate_list[0]])
+    DD_votes = int(votes[candidate_list[1]])
+    RAD_votes = int(votes[candidate_list[2]])
 
-    vote_display = [CCS_votes, DD_votes, RAD_votes]
+    vote_candidates = (CCS_votes, DD_votes, RAD_votes)
+    winner_votes = max(vote_candidates)
 
-#"{:.2f}".format(13.949999999999999)
     #The percentage of votes each candidate won 
-    CCS_percent = (float(CCS_votes)/float(total_votes))*100
-    DD_percent = (float(DD_votes)/float(total_votes))*100 
-    RAD_percent = (float(RAD_votes)/float(total_votes))*100
-
-    percent_display = [CCS_percent, DD_percent, RAD_percent]
-
-display = zip(candidate_list, percent_display, vote_display)
-vote_summary = list(display)
-
-
-#* The winner of the election based on popular vote.
-
+    CCS_percent = ((int(CCS_votes)/int(total_votes))*100)
+    DD_percent = (int(DD_votes)/int(total_votes))*100 
+    RAD_percent = (int(RAD_votes)/int(total_votes))*100
+    percents = (CCS_percent, DD_percent, RAD_percent)
+    #format the percents by reducing decimals and adding percent symbol
+    percent_candidates = list(map("{:.3f}%".format, percents))
+    #spot check percents
+    #print(percent_candidates)
+#create summary with everything together
+numbers_display = zip(candidate_list, percent_candidates, vote_candidates)
+vote_summary = list(numbers_display)
+       
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
-print(f"{vote_summary}")
+print(*vote_summary,sep='\n')
 print("-------------------------")
 #print(f"Winner: {winner}") 
 print("-------------------------")
